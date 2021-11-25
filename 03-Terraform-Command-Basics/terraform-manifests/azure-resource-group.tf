2314eb4678af058c0d3b7cbcda69765dd1b4ac76
satyaswarup swain <satyaswarup.inbox@gmail.com>
2021-11-25 19:09:49 +0530

    change resource group name

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
+resource "azurerm_resource_group" "my_azure-rg1" {
   location = "eastus"
-  name = "my-demo-rg1"  
+  name = "my-azure-rg1"  
 }
